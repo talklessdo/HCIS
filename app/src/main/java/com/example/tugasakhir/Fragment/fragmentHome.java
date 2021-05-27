7b408dbfaa94b6a1016ee467502974fd0049cc47
Sukma Hidayatullah <hidayatullahsukma@gmail.com>
2021-05-27 21:58:44 +0700

    tambah activity daftar kehadiran,sakit,izin,cuti hari ini

--- app/src/main/java/com/example/tugasakhir/Fragment/fragmentHome.java
+++ app/src/main/java/com/example/tugasakhir/Fragment/fragmentHome.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import com.example.tugasakhir.Helper.UserManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.tugasakhir.R;
 import com.example.tugasakhir.cuti_today;
@@ -23,10 +25,13 @@ import com.example.tugasakhir.sakit_today;
  */
 public class fragmentHome extends Fragment {
 
-    CardView sakit;
-    CardView hadir;
-    CardView izin;
-    CardView cuti;
+    CardView list_sakit;
+    CardView list_hadir;
+    CardView list_izin;
+    CardView list_cuti;
+    TextView userName;
+    // Required empty public constructor
+    UserManager userManager;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -38,7 +43,7 @@ public class fragmentHome extends Fragment {
     private String mParam2;
 
     public fragmentHome() {
-        // Required empty public constructor
+
     }
 
     /**
@@ -65,37 +70,48 @@ public class fragmentHome extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_fitur,container,false);
+        View v = inflater.inflate(R.layout.fragment_home,container,false);
+        list_hadir = v.findViewById(R.id.card_hadir);
+        list_sakit = v.findViewById(R.id.card_sakit);
+        list_izin = v.findViewById(R.id.card_izin);
+        list_cuti = v.findViewById(R.id.card_cuti);
 
-        hadir.setOnClickListener(new View.OnClickListener() {
+        userManager = new UserManager(getContext());
+        userName = v.findViewById(R.id.name);
+
+        userName.setText(userManager.getData().getNama());
+
+        list_hadir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(), hadir_today.class));
             }
         });
 
-        sakit.setOnClickListener(new View.OnClickListener() {
+        list_sakit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(), sakit_today.class));
             }
         });
 
-        cuti.setOnClickListener(new View.OnClickListener() {
+        list_cuti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(), cuti_today.class));
             }
         });
 
-        izin.setOnClickListener(new View.OnClickListener() {
+        list_izin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(), izin_today.class));
